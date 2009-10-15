8d5b874676b22167104a9f4a057cba86d31aa926
ng.sousa <ng.sousa@885dddfa-ac73-11de-a86f-b10fb6d3a35f>
2009-10-15 15:12:00 +0000

    PSC Exercicio 3 - final

--- trunk/PSC/Series/Serie-01/serie01-e03.c
+++ trunk/PSC/Series/Serie-01/serie01-e03.c
@@ -1,23 +1,68 @@
+/**
+ * Primeira série de exercícios - Inverno de 2009/10
+ * Grupo 1:
+ * -> 30896: Ricardo Canto
+ * -> 31401: Nuno Cancelo
+ * -> 33595: Nuno Sousa
+ * 
+ * Exercicio 3:
+ * São utilizadas quinze funções para dividir o problema e facilitar a leitura:
+ * -> processInput(), processG(), processT(), processR()
+ * 	  Funções que lêem o ficheiro e processam os 3 tipos de linhas existentes
+ * 	  construindo os arrays com dados importados.
+ * 	  As funções que processam os dados baseam-se no formato actual do ficheiro. Se
+ *    este formato for alterado apenas é necessário actualizar os indices de início
+ *    dos campos que queremos importar.
+ * 	  Ao processar as reservas verifica se a reserva foi efectuada numa turma válida
+ * 
+ * -> processArgs(char * type, char * filter)
+ *    Processa os argumentos passados ao programa. Se não tiver argumentos ou o 
+ *    primeiro argumento for h (ou H) mostra a ajuda. Se o primeiro argumento for 
+ *    U (Unidade Curricular), D (Docente) ou T(turma) chama as funções que vão aplicar
+ *    os filtros para a listagem a mostrar.
+ * 
+ * -> listU(char * filter), listD(char * filter), listG(char * filter)
+ * 	  filtra os dados de acordo co ma listagem pedida pelo utilizador. Caso o 2º
+ *    argumento não seja indicado mostra a lista de Unidades Curriculares, Docentes ou
+ * 	  Grupos de turmas disponíveis para pesquisa.
+ *    Caso exista 2º argumento produz a lista de acordo com o filtro indicado.
+ * 
+ * -> findU(char * str, int max), findD(char * str, int max), findG(char * str, int max)	
+ *    As funções find são utilizadas para não repetir dados quando se mostra ao utilizador
+ * 	  as listagens de filtros disponíveis.
+ * 
+ * ->addAluno(unsigned int num)
+ * 	  Quando encontra um aluno que respeite os filtros indicados este é adicionado à lista
+ * 	  final ordenados.
+ * 
+ * -> findTurma(char * str)
+ *    Para processamento das Reservas procura a turma a que cada aluno se quer inscrever
+ * 	  para adicionar o ponteiro para a turma correcta na estrutura de Reservas.
+ * 
+ * -> showList() e showHelp()
+ * 	  Funções que mostram a ajuda para utilização do programa na linha de comandos e que 
+ * 	  mostra a lista pedida após processamento.
+ *  
+ * Estratégia de resolução: 
+ * -> Partiu-se o problema em duas partes.
+ * -> Uma parte que gere o input recebido, e alimenta as estruturas definidas com os dados
+ * -> Uma parte que processa os argumentos, constrói a listagem pedida e apresenta a listagem
+ *    ao utilizador
+ */ 
+
+
 #include <stdio.h>
 #include <string.h>
 
-/**
-   Exercicio 3:
-	Considerando as estruturas apresentadas escreva um programa que leia, via standard input, a informação sobre as turmas e respectivas reservas para PSC, PICC e CPg, disponivel no ficheiro de texto anexo. Considere que não existem mais do que 10 turmas nem mais do que 250 alunos. O programa deverá, de seguida, apresentar um menu de opções que permita apresentar listagens de alunos de acordo com (pelo menos) os seguintes critérios:
-	- todos os alunos de uma dada unidade curricular;
-	- todos os alunos de um dado docente;
-	- todos os alunos das turmas agrupadas na mesma sala e horario (campo grpt de InfoTurma).
-*/
-
 #define MAX_TURMAS 10
 #define MAX_ALUNOS 250
-
 #define MAX_AB_DOCENTE 4
 #define MAX_AB_UNCURR 5
 #define MAX_AB_TURMA 6
 
 int idxT=0;
 int idxR=0;
+int idxA=0;
 
 typedef struct info_turma {
 	char docente[4];	/*Abreviatura: JHT, PAP, MCS, JPP, JMP */
@@ -32,51 +77,16 @@ typedef struct reserva {
 } Reserva;
 
 InfoTurma turmas[MAX_TURMAS];
-Reserva reservas[250];
+Reserva reservas[MAX_ALUNOS];
+unsigned int listaAlunos[MAX_ALUNOS];
 
-void addDocente() {
-	int i,c;
-
-	for (i=0; (c=getchar())!=' '; ++i) {
-		turmas[idxT].docente[i]=c;
-	}
-	turmas[idxT].docente[i]=0;
-
-	while ((c=getchar()) != ' ');
-	turmas[idxT].grpt=getchar();
-	
-	turmas[idxT].unCurr[0] ='0';
-	turmas[idxT].turma[0]='0';
-	/*printf("%s - %c \n",turmas[idxT].docente,turmas[idxT].grpt);*/
-	idxT++;
-}
-
-void addTurma() {
-	int i,c, j=0;
-	char turmatmp[6], grp;
-	
-	for (i=0; (c=getchar())!=' '; ++i) {
-		turmatmp[i]=c;
-	}
-	turmatmp[i]=0;	
-	grp = getchar();
-	
-	for (i=0; i<idxT; ++i){
-		if (turmas[i].grpt==grp){
-			if (turmas[i].unCurr[0] != '0'){
-				strcpy(turmas[idxT].docente, turmas[i].docente);
-				turmas[idxT].grpt = turmas[i].grpt;
-				i=idxT++;
-			}
-			getchar();
-			while ((c=getchar())!='\n' && c!='\r' && c!=EOF){
-				turmas[i].unCurr[j++]=c;
-			}
-			turmas[i].unCurr[j]=0;
-			strcpy(turmas[i].turma, turmatmp);			
-			break;
-		}
-	}
+void addAluno(unsigned int num){
+	int i=0,j;
+	while(i<idxA && num>listaAlunos[i])
+		++i;
+	for(j=++idxA; j>i; --j)
+		listaAlunos[j]=listaAlunos[j-1];
+	listaAlunos[i]=num;
 }
 
 InfoTurma * findTurma(char * str){
@@ -88,95 +98,220 @@ InfoTurma * findTurma(char * str){
 	return NULL;
 }
 
-void addReserva(){
-	int c,i;
-	char turmatmp[6];
-	unsigned int number=0;
-	
-	for (i=0; (c=getchar())!=' '; ++i) {
-		turmatmp[i]=c;
+void processG(){
+	char c;
+	int i=0,a=0;
+	while((c=getchar()) != '\n'  && c != EOF && c!=0 && c!='\r'){
+		
+		if (i>0 && i<4){
+			turmas[idxT].docente[a]=c;
+			a++;
+		}else if (i == 5){
+			
+		}else if (i == 7){
+			turmas[idxT].grpt=c;
+		}
+		++i;
 	}
-	turmatmp[i]=0;
-	while ((c=getchar())>='0' && c <='9'){
-		number=number*10 +(c-'0');
-	}
-	reservas[idxR].numAluno = number;
-	reservas[idxR++].pTurma = findTurma(turmatmp);	/*validar no caso de nao encontrar a turma*/
+	turmas[idxT].unCurr[0]=0;
+	turmas[idxT].turma[0]=0;
+	idxT++;
 }
 
-void processFile(){
-	int c;
-
-	while((c=getchar()) !=  EOF){
-		switch (c) {
-			case 'G':
-				getchar();
-				addDocente();
-			break;
-			case 'T':
-				getchar();
-				addTurma();
-			break;
-			case 'R':
-				getchar();			
-				addReserva();
-			break;
-		}	
+void processT(){
+	char c;
+	int i=0,a=0,b=0;
+	char turmatmp[MAX_AB_TURMA], grp;
+	char unCurrtmp[MAX_AB_UNCURR];
+	
+	while((c=getchar()) != '\n'  && c != EOF && c!=0 && c!='\r'){
+		if (i>0 && i<6){
+			turmatmp[a]=c;
+			a++;
+		}else if (i == 7){
+			grp=c;
+		}else if (i > 8){
+			unCurrtmp[b]=c;
+			b++;
+		}
+		++i;
 	}
+	
+	turmatmp[a]=0;
+	unCurrtmp[b]=0;
+
+	
+	
+	for (i=0; i<idxT; ++i){
+		if (turmas[i].grpt==grp){
+			 if (turmas[i].unCurr[0] != 0){
+				
+				
+				strcpy(turmas[idxT].docente, turmas[i].docente);
+				turmas[idxT].grpt = turmas[i].grpt;
+				i=idxT++;
+			}
+			
+			strcpy(turmas[i].turma,turmatmp);	
+			
+			strcpy(turmas[i].unCurr,unCurrtmp);
+			break;
+
+		}
+	}
+
+}
+
+void processR(){
+	char c;
+	int i=0,a=0;
+	char turmatmp[MAX_AB_TURMA];
+	unsigned int val=0;
+	InfoTurma * InfoTurmatmp;
+	
+	while((c=getchar()) != '\n'  && c != EOF && c!=0 && c!='\r'){
+		
+		if (i>0 && i<6){
+			turmatmp[a]=c;
+			a++;
+		}else if (i > 6 && c >='0' && c <='9'){
+			val=val*10 +(c-'0');
+		}
+		i++;
+	}
+	turmatmp[a] = 0;
+	
+	if ((InfoTurmatmp=findTurma(turmatmp))!=NULL){
+		reservas[idxR].pTurma = InfoTurmatmp;	/*validar no caso de nao encontrar a turma*/
+		reservas[idxR].numAluno = val;
+		idxR++;
+	}else
+		printf("A reserva do aluno nº %d foi efectuada numa turma não válida (%s)\n",val,turmatmp);
+}
+
+void processInput(){
+	/**
+	 *  0 1 2 3 4 5 6 7 8
+	 * |_|_|_|_|_|_|_|_|_|
+	 *  G   P A P   D   1
+	 * 
+	 *  0 1 2 3 4 5 6 7 8 910111213
+	 * |_|_|_|_|_|_|_|_|_|_|_|_|_|_|
+	 *  T   L I 3 1 D   1   P I C C
+	 * 
+	 *  0 1 2 3 4 5 6 7 8 910111213
+	 * |_|_|_|_|_|_|_|_|_|_|_|_|_|
+	 *  R   L I 3 1 D   3 1 4 0 1 
+	 * */
+	char c;
+	
+	do{
+		c=getchar();
+		switch(c){
+				case 'G':
+					processG();
+					break;
+				case 'T':
+					processT();
+					break;
+				case 'R':
+					processR();
+					break;
+		}
+	}while(c!=EOF);
+}
+
+int findU(char * str, int max){
+	int i;
+	for (i=0; i<max; ++i)
+		if (strcmp(turmas[i].unCurr,str)==0)
+			return i;
+	return -1;
+}
+
+int findD(char * str, int max){
+	int i;
+	for (i=0; i<max; ++i)
+		if (strcmp(turmas[i].docente,str)==0)
+			return i;
+	return -1;
+}
+
+int findG(int grp, int max){
+	int i;
+	for (i=0; i<max; ++i)
+		if (turmas[i].grpt==grp)
+			return i;
+	return -1;
+}
+
+void showList(){
+	int i;
+	if (idxA==0) puts("\n Não existem alunos para listar de acordo com as condições indicadas.\n");
+	for(i=0; i<idxA;++i)
+		printf("%d\n", listaAlunos[i]);
 }
 
 void listU(char * filter){
 	int i;
 	
-	if (filter == NULL)
+	if (filter == NULL){
 		for (i=0; i<idxT; ++i)
-			printf("%s\n",turmas[i].unCurr);
-	else
+			if (findU(turmas[i].unCurr, i)==-1)
+				printf("%s\n",turmas[i].unCurr);
+	} else {
+		printf("Lista de alunos da Unidade Curricular: %s \n",filter); 
 		for (i=0; i<idxR; ++i)
 			if (strcmp((*reservas[i].pTurma).unCurr,filter) == 0)
-				printf("%d - %s\n", reservas[i].numAluno, (*reservas[i].pTurma).turma);
+				addAluno(reservas[i].numAluno);
+		showList();
+	}
 }
 
 void listD(char * filter){
 	int i;
 	
-	if (filter == NULL)
+	if (filter == NULL){
 		for (i=0; i<idxT; ++i)
-			printf("%s \n",turmas[i].docente);
-	else
+			if (findD(turmas[i].docente, i)==-1)
+				printf("%s \n",turmas[i].docente);
+	} else {
+		printf("Lista de alunos do Docente: %s \n",filter); 
 		for (i=0; i<idxR; ++i)
 			if (strcmp((*reservas[i].pTurma).docente,filter) == 0)
-				printf("%d - %s\n", reservas[i].numAluno, (*reservas[i].pTurma).unCurr);	
+				addAluno(reservas[i].numAluno);
+		showList();
+	}
 }
 
 void listG(char * filter){
 	int i;
 	
-	if (filter == NULL)
+	if (filter == NULL){
 		for (i=0; i<idxT; ++i)
-			printf("%c \n",turmas[i].grpt);
-	else
+			if (findG(turmas[i].grpt, i)==-1)
+				printf("%c \n",turmas[i].grpt);
+	}else {
+		printf("Lista de alunos das turmas do grupo: %s \n",filter); 
 		for (i=0; i<idxR; ++i)
 			if ((*reservas[i].pTurma).grpt == (*filter))
-				printf("%d - %s\n", reservas[i].numAluno, (*reservas[i].pTurma).unCurr);			
+				addAluno(reservas[i].numAluno);
+		showList();
+	}		
 }
 
 void processArgs(char * type, char * filter){
-
 	switch (*type) {
 		case 'U':		/*filtrar por unidade curricular*/
 		case 'u':
-			printf("Lista de alunos da Unidade Curricular: %s \n",filter); 
 			listU(filter);
 		break;
 		case 'D':		/*filtrar por Docente*/
 		case 'd':
-			printf("Lista de alunos do Docente: %s \n",filter); 
 			listD(filter);
 		break;
 		case 'G':		/*filtrar por grupo horario*/
 		case 'g':
-			printf("Lista de alunos das turmas do grupo: %s \n",filter); 
 			listG(filter);
 		break;
 		default:
@@ -200,24 +335,18 @@ void showHelp(){
 }
 
 int main(int argc, char *argv[]){
-/*	int i;*/
 
 	if (argc<2){
-		puts("Need at least one argument to be searched!");
-		puts("Use H or h as first argument for details.");
+		puts("\n***Need at least one argument to be searched!***\n");
+		showHelp();
 	} else {
 		if (*argv[1] == 'h' || *argv[1] == 'H') {
 			showHelp();
 			return 0;
 		}
-		processFile();
+		processInput();
 		processArgs(argv[1], argv[2]);
 	}
 	
-/*	for (i=0; i<idxT;++i)
-		printf("%s - %s - %s - %c \n",turmas[i].docente,turmas[i].unCurr,turmas[i].turma,turmas[i].grpt);
-	for (i=0; i<idxR;++i){
-		printf("%d - %s  \n", reservas[i].numAluno,(*reservas[i].pTurma).turma);
-	}*/
 return 0;
 }
