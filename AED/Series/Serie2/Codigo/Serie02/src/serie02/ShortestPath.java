240fa024e4488ef1f990fefaf456593a62b62601
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@885dddfa-ac73-11de-a86f-b10fb6d3a35f>
2009-11-04 12:07:14 +0000

    AED: Correcção do Ex. 1 e 3 Inicio do 4.

--- AED/Series/Serie2/Codigo/Serie02/src/serie02/ShortestPath.java
+++ AED/Series/Serie2/Codigo/Serie02/src/serie02/ShortestPath.java
@@ -5,7 +5,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.StringTokenizer;
@@ -17,28 +20,132 @@ import java.util.StringTokenizer;
  * -> 33595: Nuno Sousa
  */
 public class ShortestPath {
-    private final String PATH="/home/masterzdran/Desktop/Link to ISEL/semestre3/AED/Series/Serie2/";
-    private final String FILENAME=PATH+"USA-road-d.NY.gr";
 
+    private static final String PATH = "D:/ISEL/Trabalhos/AED/Series/Serie2/";
+    private static final String FILENAME = PATH + "USA-road-d.NY.gr";
+    private final int MAX_LINES = 10000;
+    private File filename;
+    private PathNode[] pna = new PathNode[MAX_LINES];
+    private int lineNbrAll = 0;
 
-    public void openFile(File filename){
+    public ShortestPath(File filename) {
+        this.filename = filename;
+    }
+
+    public void openFile() {
         BufferedReader fr;
         BufferedWriter fw;
 
         try {
             fr = new BufferedReader(new FileReader(filename));
-            String xpto=fr.readLine();
-            StringTokenizer ups= new StringTokenizer(xpto, xpto)
+            fw = new BufferedWriter(new FileWriter(filename + ".new"));
 
-            
+            read(fr);
+
+            //mySort(pna, new orderPathByWeight());
+
+            //write(fw);
+
+            fw.close();
+            fr.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ShortestPath.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(ShortestPath.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        
-
     }
 
+    private void read(BufferedReader fr) throws IOException {
+        StringTokenizer ups;
+        String type ;
+        String line;
+        PathNode pn;
+        int rowNbr=0;
+        while ((line = fr.readLine()) !=  null  && (rowNbr < MAX_LINES)){
+                ups = new StringTokenizer(line, " ");
+                type= (String) ups.nextElement();
+
+                //if (type.equals("a")) {
+
+//                    int tail = Integer.parseInt((String) ups.nextElement());
+//                    int head = Integer.parseInt((String) ups.nextElement());
+//                    int weight = Integer.parseInt((String) ups.nextElement());
+//                    pn = new PathNode('a', tail, head, weight);
+//                    //pna[lineNbr++] = pn;
+
+        }
+    }
+
+    private void write(BufferedWriter fw) throws IOException {
+        int lnNbr = 0;
+        while (lnNbr < MAX_LINES) {
+            fw.append(pna[lnNbr++].toString() + "\n");
+        }
+    }
+
+    private void mySort(PathNode[] p, Comparator<PathNode> cmp) {
+        Arrays.sort(p, cmp);
+    }
+
+    public static void main(String[] args) {
+        ShortestPath sp = new ShortestPath(new File(FILENAME));
+        sp.openFile();
+    }
+}
+
+class PathNode implements Comparable<PathNode> {
+
+    private char arc;
+    private int tail;
+    private int head;
+    private int weight;
+
+    public PathNode(char a, int t, int h, int w) {
+        arc = a;
+        tail = t;
+        head = h;
+        weight = w;
+    }
+
+    public int getHead() {
+        return head;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return arc + " " + tail + " " + head + " " + weight;
+    }
+
+    public int compareTo(PathNode o) {
+        return this.weight - o.weight;
+    }
+}
+
+class orderPathByTail implements Comparator<PathNode> {
+
+    public int compare(PathNode o1, PathNode o2) {
+        return o1.getTail() - o2.getTail();
+    }
+}
+
+class orderPathByHead implements Comparator<PathNode> {
+
+    public int compare(PathNode o1, PathNode o2) {
+        return o1.getHead() - o2.getHead();
+    }
+}
+
+class orderPathByWeight implements Comparator<PathNode> {
+
+    public int compare(PathNode o1, PathNode o2) {
+        return o1.getWeight() - o2.getWeight();
+    }
 }
